18d6bba52bd3b065e00e0eeaf8dbab1d39e9d0c0
oubakiou <oubakiou@gmail.com>
2022-05-12 17:25:06 +0900

    apollo server

---
+++ v0.1.1/helloworld-app/pages/api/graphql.ts
@@ -0,0 +1,80 @@
+import { ApolloServer, Config, gql } from 'apollo-server-micro'
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+} from 'apollo-server-core'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// スキーマ定義(インターフェース)
+const typeDefs: Config['typeDefs'] = gql`
+  type Query {
+    statuses: [Status]!
+  }
+
+  type Status {
+    id: String!
+    body: String!
+    author: String!
+    createdAt: String!
+  }
+`
+
+// スキーマを実際に動作させるリゾルバー(実装)
+const resolvers: Config['resolvers'] = {
+  Query: {
+    statuses() {
+      return listStatuses()
+    },
+  },
+}
+const listStatuses = (): Status[] => statuses
+
+// ハードコーディングされたデータ
+type Status = { id: string; body: string; author: string; createdAt: string }
+const statuses: Status[] = [
+  {
+    id: '2',
+    body: 'inviting coworkers',
+    author: 'jack',
+    createdAt: new Date(2021, 4, 2).toISOString(),
+  },
+  {
+    id: '1',
+    body: 'just setting up my app',
+    author: 'jack',
+    createdAt: new Date(2021, 4, 1).toISOString(),
+  },
+]
+
+// Next.jsのAPI Routeの設定
+// @see https://nextjs.org/docs/api-routes/api-middlewares#custom-config
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+// Apollo Serverの生成
+const isDevelopment = process.env.NODE_ENV === 'development'
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: isDevelopment,
+  plugins: [
+    isDevelopment
+      ? ApolloServerPluginLandingPageGraphQLPlayground()
+      : ApolloServerPluginLandingPageDisabled(),
+  ],
+})
+
+// 起動と登録
+const startServer = apolloServer.start()
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
